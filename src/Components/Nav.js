8bbb03cc17ec002a212199472bc0a162a9a5d908
ctang415 <carrietang117@gmail.com>
2023-02-26 00:42:59 -0500

    Added logo and icons to nav bar

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,14 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { ShoppingCart } from "./Images";
 
 const Nav = () => {
   return (
     <nav className="nav-bar">
-      <h3>Logo</h3>
+      <div><h3>The Burnt Wick</h3></div>
       <ul>
-        <Link to="/"><li>Home</li></Link>
-        <Link to="/store"><li>Store</li></Link>
-        <Link to="/checkout">Checkout</Link>
+        <div className="nav-links">
+          <Link to="/"><li>Home</li></Link>
+          <Link to="/store"><li>Store</li></Link></div>
+        <div className="nav-cart">
+          <Link to="/checkout">
+            <div><img className="nav-cart-image" alt="Shopping cart" src={ShoppingCart}></img>(0)</div>
+            </Link>
+        </div>
       </ul>
     </nav>
   );
